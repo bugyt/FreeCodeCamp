ce92395fd7c2ab99d76990b0804959d1d0aa1b83
Laurent H <bugyt@users.noreply.github.com>
2016-03-03 14:55:50 +0100

    Update Where_art_thou.js

--- Intermediate-Algorithm-Scripting/Where_art_thou.js
+++ Intermediate-Algorithm-Scripting/Where_art_thou.js
@@ -1,3 +1,9 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Where art thou :
+// Make a function that looks through an array of objects (first argument) and returns an array of all objects that have 
+// matching property and value pairs (second argument). Each property and value pair of the source object has to be present in 
+// the object from the collection if it is to be included in the returned array.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function where(collection, source) {
   var arr = [];
   var keys = Object.keys(source);
